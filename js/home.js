6d1ab3a2c572c9b7126c5a36e4464fce3851845c
Ant1997 <anthonyhuynh997@gmail.com>
2021-03-13 23:25:38 -0600

    Finished.

--- js/home.js
+++ js/home.js
@@ -1,38 +1,49 @@
 $(document).ready(function () {
-    var addedMoney = 0;
+    addedMoney = 0;
+    selectedItemId = null;
+
     loadItems();
     $('#addDollar').on('click', function () {
-            addedMoney += 1;
+            addedMoney += 1.000;
             messageBox("You added $1.00");
             updateMoney(addedMoney);
         });
         $('#addQuarter').on('click', function () {
-            addedMoney += .25;
+            addedMoney += 0.250;
             messageBox("You added $0.25")
             updateMoney(addedMoney);
         });
         $('#addDime').on('click', function () {
-            addedMoney += .10;
+            addedMoney += 0.100;
             messageBox("You added $0.10");
             updateMoney(addedMoney);
         });
         $('#addNickel').on('click', function () {
-            addedMoney += .05;
+            addedMoney += 0.050;
             messageBox("You added a $0.05")
             updateMoney(addedMoney);
         });
+
+    purchaseClicked();
 
 });
 
 
 function updateMoney(money) {
+    money = parseFloat(money);
     $('#moneyInput').empty();
-    $('#moneyInput').val(money.toFixed(2));
+    $('#moneyInput').val(formatToCurrency(money));
+    //addedMoney = $('#moneyInput').val();
 }
 
 function messageBox(messageResponse) {
     $('#messageOutput').empty();
     $('#messageOutput').val(messageResponse);
+}
+
+function changeBox(change) {
+    $('#changeOutput').empty();
+    $('#changeOutput').val(change);
 }
 
 function clearErrorMessage(){
@@ -45,8 +56,14 @@ const formatToCurrency = amount => {
   return "$" + amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 };
 
+function clearItemsTable() {
+    $('#gridContentCol1').empty();
+    $('#gridContentCol2').empty();
+    $('#gridContentCol3').empty();
+}
+
 function loadItems() {
-    //clearItemsTable();
+    clearItemsTable();
     var grid1 = $('#gridContentCol1');
     var grid2 = $('#gridContentCol2');
     var grid3 = $('#gridContentCol3');
@@ -58,10 +75,10 @@ function loadItems() {
             $.each(itemArray, function(index, item){
                 var id = item.id;
                 var name = item.name;
-                var price = item.price;
+                var price = parseFloat(item.price);
                 var quantity = item.quantity;
 
-                var card = '<div class="card">';
+                var card = '<div class="card" onclick="setId(' + id + ')">';
                     card += '<div class="card-body" style="height:283px">';
                     card += '<p style="font-weight:900">' + id + '</p>';
                     card += '<p style="text-align:center; font-weight:900">' + name + '</p>';
@@ -89,6 +106,74 @@ function loadItems() {
         },
         error: function() {
             messageBox("Invalid item selected");
+        }
+    });
+}
+
+function setId(id){
+    selectedItemId = id;
+    //alert(selectedItemId);
+    messageBox("Item ID " + selectedItemId + " selected");
+    $('#itemChosen').empty();
+    $('#itemChosen').val(selectedItemId);
+}
+
+function purchaseClicked(){
+    $('#purchaseButton').on('click', function () {
+
+        if(selectedItemId == null){
+            messageBox("Please make a selection");
+        }
+        else{
+
+            //alert(amount);
+            $.ajax({
+                    type: 'POST',
+                    url: 'http://tsg-vending.herokuapp.com/money/'+ addedMoney.toFixed(2) +'/item/' + selectedItemId,
+                    success: function(item) {
+
+                       var quarters = item['quarters'];
+                       var dimes = item['dimes'];
+                       var nickels = item['nickels'];
+                       var pennies = item['pennies'];
+                       var money = parseFloat(0);
+
+                       var changeText = "";
+
+                       if (quarters > 0){
+                            changeText += quarters + " Quarter ";
+                            money += quarters * 0.25;
+                       }
+                       if (dimes > 0){
+                            changeText += dimes + " Dime ";
+                            money += dimes * 0.10;
+                       }
+                       if (nickels > 0){
+                            changeText += nickels + " Nickel ";
+                            money += nickels * 0.05;
+                       }
+                       if (pennies > 0){
+                            changeText += pennies + " Penny ";
+                            money += pennies * 0.01;
+                       }
+                       changeBox(changeText);
+
+                       addedMoney = money;
+                       money = parseFloat(money);
+                       alert(money);
+                       updateMoney(money);
+                       loadItems();
+                       messageBox("Thank you!!");
+                       selectedItemId = null;
+                       $('#itemChosen').empty();
+                       //loadItems();
+                       //subtract deposited money
+
+                    },
+                    error: function(xhr, status, error) {
+                        messageBox(xhr.responseJSON['message']);
+                    }
+            });
         }
     });
 }
